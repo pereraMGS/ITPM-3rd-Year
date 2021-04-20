ae8cded4383945c5e61b13b9b97c9288dc10b8c6
pereraMGS <gimhani99@icloud.com>
2021-04-21 00:58:58 +0530

    Second Commit

--- ITPM/src/module-info.java
+++ ITPM/src/module-info.java
@@ -3,6 +3,8 @@ module ITPM {
 	requires javafx.graphics;
 	requires javafx.fxml;
 	requires javafx.base;
+	requires java.sql;
+	requires java.desktop;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
